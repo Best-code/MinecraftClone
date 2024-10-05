463645310310e1d6db3f405d41197e0bc1ab1008
Best-code <cpmraygun@gmail.com>
2024-10-04 21:12:17 -0400

    Initial Commit

---
+++ MineCraftClone/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MineCraftClone
+//
+//  Created by Colin Maloney on 10/4/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
